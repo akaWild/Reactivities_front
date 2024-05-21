7fc65141350b29758ff4e39798156a3bb3aa9222
akaWild <nemyx84@gmail.com>
2024-05-21 17:45:22 +0300

    Added ActivityForm prototype and put it inside ActivityDashboard component

--- src/features/activities/dashboard/ActivityDashBoard.tsx
+++ src/features/activities/dashboard/ActivityDashBoard.tsx
@@ -2,6 +2,7 @@ import { Grid, GridColumn } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
   activities: Activity[];
@@ -13,7 +14,10 @@ function ActivityDashBoard({ activities }: Props) {
       <Grid.Column width="10">
         <ActivityList activities={activities} />
       </Grid.Column>
-      <GridColumn width="6">{activities[0] && <ActivityDetails activity={activities[0]} />}</GridColumn>
+      <GridColumn width="6">
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
+        <ActivityForm />
+      </GridColumn>
     </Grid>
   );
 }
