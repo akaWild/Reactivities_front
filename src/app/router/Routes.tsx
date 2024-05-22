5b1642c7467cbecc0fc530e40a7fc1df5687828f
akaWild <nemyx84@gmail.com>
2024-05-22 16:04:13 +0300

    Added child routes to the Routes component

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,10 +1,18 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
+import HomePage from "../../features/home/HomePage";
+import ActivityDashBoard from "../../features/activities/dashboard/ActivityDashBoard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "activities", element: <ActivityDashBoard /> },
+      { path: "createActivity", element: <ActivityForm /> },
+    ],
   },
 ];
 
