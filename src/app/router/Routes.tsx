c8b0ab4ce5a695ef03f92c938299fcfa32e1e324
akaWild <nemyx84@gmail.com>
2024-05-23 01:22:53 +0300

    Added navigation to NotFound page in case of unexisting url

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,10 +1,11 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashBoard from "../../features/activities/dashboard/ActivityDashBoard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
 
 export const routes: RouteObject[] = [
   {
@@ -17,6 +18,16 @@ export const routes: RouteObject[] = [
       { path: "createActivity", element: <ActivityForm key="create" /> },
       { path: "manage/:id", element: <ActivityForm key="manage" /> },
       { path: "errors", element: <TestErrors /> },
+      { path: "not-found", element: <NotFound /> },
+      {
+        path: "*",
+        element: (
+          <Navigate
+            replace
+            to="/not-found"
+          />
+        ),
+      },
     ],
   },
 ];
