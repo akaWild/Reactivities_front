210b3247cc5e2e1f8272a014b23fa56d4cfa3b09
akaWild <nemyx84@gmail.com>
2024-05-24 17:26:45 +0300

    Updated home page and nav bar to show logged in user

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
 function HomePage() {
+  const { userStore } = useStore();
+
   return (
     <Segment
       inverted
@@ -19,21 +23,33 @@ function HomePage() {
             style={{ marginBottom: 12 }}
           />
         </Header>
-        <Header
-          as="h2"
-          inverted
-          content="Welcome to reactivities"
-        />
-        <Button
-          as={Link}
-          to="/login"
-          size="huge"
-          inverted>
-          Login
-        </Button>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header
+              as="h2"
+              inverted
+              content="Welcome to reactivities"
+            />
+            <Button
+              as={Link}
+              to="/activities"
+              size="huge"
+              inverted>
+              Go to Activities!
+            </Button>
+          </>
+        ) : (
+          <Button
+            as={Link}
+            to="/login"
+            size="huge"
+            inverted>
+            Login!
+          </Button>
+        )}
       </Container>
     </Segment>
   );
 }
 
-export default HomePage;
+export default observer(HomePage);
