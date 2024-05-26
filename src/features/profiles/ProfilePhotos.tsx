0046cbfd9653d7b002ba6b424d4383574c100668
akaWild <nemyx84@gmail.com>
2024-05-26 21:10:33 +0300

    Added conditionally rendering photo widget (stub)

--- src/features/profiles/ProfilePhotos.tsx
+++ src/features/profiles/ProfilePhotos.tsx
@@ -1,25 +1,51 @@
 import { observer } from "mobx-react-lite";
-import { Card, Header, Image, TabPane } from "semantic-ui-react";
+import { Button, Card, Grid, GridColumn, Header, Image, TabPane } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
 
 interface Props {
   profile: Profile;
 }
 
 function ProfilePhotos({ profile }: Props) {
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const {
+    profileStore: { isCurrentUser },
+  } = useStore();
+
   return (
     <TabPane>
-      <Header
-        icon="image"
-        content="Photos"
-      />
-      <Card.Group itemsPerRow={5}>
-        {profile.photos?.map((photo) => (
-          <Card key={photo.id}>
-            <Image src={photo.url} />
-          </Card>
-        ))}
-      </Card.Group>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header
+            floated="left"
+            icon="image"
+            content="Photos"
+          />
+          {isCurrentUser && (
+            <Button
+              floated="right"
+              basic
+              content={addPhotoMode ? "Cancel" : "Add Photo "}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <GridColumn width={16}>
+          {addPhotoMode ? (
+            <p>Photo widget goes here</p>
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile.photos?.map((photo) => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </GridColumn>
+      </Grid>
     </TabPane>
   );
 }
