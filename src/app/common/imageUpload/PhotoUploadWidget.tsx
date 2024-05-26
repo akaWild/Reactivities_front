c375b25d41fa889f59b67fc044adaedbf1337206
akaWild <nemyx84@gmail.com>
2024-05-26 23:46:28 +0300

    Added photo cropping functionality via react-cropper library

--- src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,9 +1,23 @@
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Button, Grid, Header, Image } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 function PhotoUploadWidget() {
   const [files, setFiles] = useState<any>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+
+  function onCrop() {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob((blob) => console.log(blob));
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
 
   return (
     <Grid>
@@ -20,7 +34,12 @@ function PhotoUploadWidget() {
           color="teal"
           content="Step 2 - Resize image"
         />
-        {files && files.length > 0 && <Image src={files[0].preview} />}
+        {files && files.length > 0 && (
+          <PhotoWidgetCropper
+            setCropper={setCropper}
+            imagePreview={files[0].preview}
+          />
+        )}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
@@ -28,6 +47,25 @@ function PhotoUploadWidget() {
           color="teal"
           content="Step 3 - Preview & Upload"
         />
+        {files && files.length > 0 && (
+          <>
+            <div
+              className="img-preview"
+              style={{ minHeight: 200, overflow: "hidden" }}
+            />
+            <Button.Group widths={2}>
+              <Button
+                onClick={onCrop}
+                positive
+                icon="check"
+              />
+              <Button
+                onClick={() => setFiles([])}
+                icon="close"
+              />
+            </Button.Group>
+          </>
+        )}
       </Grid.Column>
     </Grid>
   );
