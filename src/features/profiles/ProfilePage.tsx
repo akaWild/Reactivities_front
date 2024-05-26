7cefda896835012b76b6cfc78689e066817d6b80
akaWild <nemyx84@gmail.com>
2024-05-26 20:55:35 +0300

    Added displaying photos at photos tab in profile page

--- src/features/profiles/ProfilePage.tsx
+++ src/features/profiles/ProfilePage.tsx
@@ -22,8 +22,12 @@ function ProfilePage() {
   return (
     <Grid>
       <Grid.Column width={16}>
-        {profile && <ProfileHeader profile={profile} />}
-        <ProfileContent />
+        {profile && (
+          <>
+            <ProfileHeader profile={profile} />
+            <ProfileContent profile={profile} />
+          </>
+        )}
       </Grid.Column>
     </Grid>
   );
